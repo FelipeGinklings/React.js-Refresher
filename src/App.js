83cad9b46cd00daed428736ac79ef43d0c9a05c8
FelipeGinklings <felipe.ginklings@gmail.com>
2023-12-10 11:23:16 -0300

    All ReactJS refresher classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [courseGoals, setCourseGoals] = useState([
+		{ id: 'cg1', text: 'Finish the Course' },
+		{ id: 'cg2', text: 'Learn all bout the Course Main Topic' },
+		{ id: 'cg3', text: 'Help other students in the Cour Q&A' },
+	]);
+
+	const addNewGoalHandler = (newGoal) => {
+		// setCourseGoals(courseGoals.concat(newGoal))
+		setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+	};
+
+	return (
+		<div className="course-goals">
+			<h2>Course Goals</h2>
+			<NewGoal onAddGoal={addNewGoalHandler} />
+			<GoalList goals={courseGoals} />
+		</div>
+	); // => React.createElement('h1', {title: 'This work'}, 'Hi, this is ReactJS')
+};
+
+// class App extends React.Component {
+// 	render() {
+// 		return (
+// 			<h1 title="This work">
+// 				Hi, <span>this</span> is ReactJS
+// 			</h1>
+// 		);
+// 	}
+// }
 
 export default App;
